26bdfce708c6e91e0e286ff14725ec69817187dc
tarif421 <tarif305@gmail.com>
2025-08-27 12:26:58 +0600

    done

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 const validPin = 1234;
+const transactionData = [];
 //function to get input values
 function getInputValueNumber(id) {
   const inputField = document.getElementById(id);
@@ -30,24 +31,29 @@ function setInnerText(value) {
 //function to get toggle
 function handleToggle(id) {
   const forms = document.getElementsByClassName("form");
+
   for (const form of forms) {
     form.style.display = "none";
   }
+
   document.getElementById(id).style.display = "block";
 }
 //function to toggle button
 function handleToggleBtns(id) {
   const formBtns = document.getElementsByClassName("form-btn");
+
   for (const btn of formBtns) {
     btn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
 
     btn.classList.add("border-gray-300");
   }
+
   document.getElementById(id).classList.remove("border-gray-300");
   document
     .getElementById(id)
     .classList.add("border-[#0874f2]", "bg-[#0874f20d]");
 }
+
 // add money feature
 document
   .getElementById("add-money-btn")
@@ -76,6 +82,12 @@ document
     const totalNewAvailableBalance = amount + availableBalance;
 
     setInnerText(totalNewAvailableBalance);
+
+    const data = {
+      name: "Add Money",
+      date: new Date().toLocaleTimeString(),
+    };
+    transactionData.push(data);
   });
 
 // cashout money feature
@@ -90,7 +102,86 @@ document.getElementById("withdraw-btn").addEventListener("click", function (e) {
   console.log(totalNewAvailableBalance);
 
   setInnerText(totalNewAvailableBalance);
+
+  const data = {
+    name: "Cash Out",
+    date: new Date().toLocaleTimeString(),
+  };
+  transactionData.push(data);
 });
+
+//transfer Money feature
+document
+  .getElementById("send-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const amount = getInputValueNumber("send-money-amount");
+
+    const availableBalance = getInnerText("available-balance");
+
+    const totalNewAvailableBalance = availableBalance - amount;
+    console.log(totalNewAvailableBalance);
+
+    setInnerText(totalNewAvailableBalance);
+  });
+//get bonus
+
+//pay bill feature
+document.getElementById("pay-bill-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const bank = getInputValue("select-pay");
+  const accountNumber = document.getElementById("biller-account-number").value;
+
+  const amount = getInputValueNumber("pay-bill-amount");
+
+  const pin = getInputValueNumber("pay-pin");
+
+  const availableBalance = getInnerText("available-balance");
+
+  if (accountNumber.length < 11) {
+    alert("Invalid Account Number");
+    return;
+  }
+
+  if (pin !== validPin) {
+    alert("invalid pin number");
+    return;
+  }
+
+  const totalNewAvailableBalance = amount - availableBalance;
+
+  setInnerText(totalNewAvailableBalance);
+});
+/////////////data
+document
+  .getElementById("transaction-card-btn")
+  .addEventListener("click", function () {
+    const transactionContainer = document.getElementById(
+      "transiction-conatainer"
+    );
+
+    for (const data of transactionData) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+       <div class=" bg-white rounded-xl p-3 flex justify-between items-center">
+                        <div class="flex  items-center mt-3">
+                            <div class=" p-3 rounded-full bg-[#f4f5f7]">
+                                <img src="assets/wallet1.png" alt="">
+                            </div>
+                            <div class="ml-3">
+                                <h1>${data.name}</h1>
+                                <p>${data.date}</p>
+                            </div>
+                        </div>
+                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                    </div>
+      `;
+
+      transactionContainer.appendChild(div);
+    }
+  });
 
 // toggling feature
 
@@ -123,4 +214,18 @@ document
     handleToggle("get-bonus-parent");
 
     handleToggleBtns("get-bonus-card-btn");
+  });
+document
+  .getElementById("pay-bill-card-btn")
+  .addEventListener("click", function () {
+    handleToggle("pay-bill-parent");
+
+    handleToggleBtns("pay-bill-card-btn");
+  });
+document
+  .getElementById("transaction-card-btn")
+  .addEventListener("click", function () {
+    handleToggle("transaction-parent");
+
+    handleToggleBtns("transaction-card-btn");
   });
